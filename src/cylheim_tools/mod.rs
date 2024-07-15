791ecf8064e2a000dfcb0f144cc969df07a0def4
ky-reflection <kyreflection@gmail.com>
2024-07-15 18:26:49 +0800

    ctd level

--- src/cylheim_tools/mod.rs
+++ src/cylheim_tools/mod.rs
@@ -5,3 +5,5 @@ pub mod cytoid_level;
 pub use cyl_config::{ChartInfo, CylheimProjectConfig, GenerateEventConfig};
 #[allow(unused)]
 pub use cyl_theme::{CylheimDiffMeta, CylheimDiffOption, CylheimThemeColor, CylheimThemeIcon};
+#[allow(unused)]
+pub use cytoid_level::{CylheimLevelMetaConfig, CytoidLevelMetaConfig};
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod cylheim_tools;
 use cylheim_tools::CylheimProjectConfig;
+use cylheim_tools::CytoidLevelMetaConfig;
+use rand::thread_rng;
 use std::fs;
+use walkdir::WalkDir;
 #[allow(dead_code)]
 
 fn main() {
-    let path = "./resource/CFHSS X.cyl";
-    let text = fs::read_to_string(path).unwrap();
-    let config: CylheimProjectConfig = serde_json::from_str(&text).unwrap();
-    println!("{:#?}", config);
+    println!("Hello, world!");
 }
